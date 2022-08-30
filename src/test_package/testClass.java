bfe019fcfc05f8c1c9119cb2c8bd52a3e7b70d19
IrodaRustamiy <107510516+IrodaRustamiy@users.noreply.github.com>
2022-08-30 10:38:36 -0500

    Update testClass.java

--- src/test_package/testClass.java
+++ src/test_package/testClass.java
@@ -3,5 +3,6 @@ package test_package;
 public class testClass {
     public static void main(String[] args) {
         System.out.println("test message");
+        // changes from a team member here
     }
 }
